0b5398325c42a3ee8a0bb36cd9ed3572d843078d
AlessioConti <alessioconti02@yahoo.it>
2022-06-09 14:53:01 +0200

    Commit di inizio lavoro, creazione classi model

---
+++ src/main/java/it/prova/gestionesocietajpamaven/GestionesocietajpamavenApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionesocietajpamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionesocietajpamavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionesocietajpamavenApplication.class, args);
+	}
+
+}
